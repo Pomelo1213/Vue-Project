f93e177865877e7e656bef9bf115b9554fa030a5
pomelo1213 <goodmanluoxin@163.com>
2020-01-25 11:08:03 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <ul class="content">
+      <li v-for="content in contentItem" :key="`content-${content}`">
+        {{ content }}
+      </li>
+    </ul>
+    <div>test</div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+export default {
+  name: "App",
+  components: {
+    "v-header": Header
+  },
+  data: function() {
+    return {
+      contentItem: ["商品", "评价", "商家"]
+    };
+  }
+};
+</script>
+
+<style lang="less">
+@import "./assets/global.less";
+.content {
+  .flex();
+
+  > li {
+    flex: 1;
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header">i am header</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.header {
+  border: 1px solid;
+}
+</style>
